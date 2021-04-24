77af469415bd8de414bbf2881cd3a2541aa773d5
Anand181-1 <73180243+Anand181-1@users.noreply.github.com>
2021-04-24 16:03:50 +0530

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,20 @@
+class System{
+
+    constructor(){
+        this.title = createElement('h2');
+    }
+
+    authenticate(actualCode,enteredCode){
+
+    this.title.html(code);
+    this.title.position(displayWidth/2 - 50, 0);
+
+
+        if(actualCode === enteredCode.toUpperCase()){
+            return true
+        }else{ 
+            return false
+        }
+    }
+
+}
